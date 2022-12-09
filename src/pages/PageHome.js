2d206674421219aca2219387eb9df191342992a9
Mike <myamauchi@my.bcit.ca>
2022-12-08 17:47:25 -0800

    working on home

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react";
+import Movies from '../components/Movies';
+import { API_KEY } from "../globals/globals";
+
 function PageHome() {
   return (
     <div>PageHome</div>
