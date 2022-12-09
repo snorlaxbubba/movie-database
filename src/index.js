061cd1b46dfc9b7e1e00dde05db4e74117575332
Mike <myamauchi@my.bcit.ca>
2022-12-09 06:42:15 -0800

    added store/toolkit to react

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRouter from './routers/AppRouter';
-// import './styles/styles.scss';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
