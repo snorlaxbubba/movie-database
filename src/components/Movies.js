3f161e5c7b96551f7efadda759bcec53d06e026b
Mike <myamauchi@my.bcit.ca>
2023-01-13 13:43:03 -0800

    added background
    background consists of spans, divs, and a ton of classes, need to work on the accessability on it with aria-hidden

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,7 @@ import MovieCard from './MovieCard';
 
 function Movies({ moviesData }) {
   return (
-    <div>
+    <div className='movie-wrapper'>
       {moviesData.map(movie => <MovieCard key={movie.id} movie={movie} />)}
     </div>
   )
--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -10,16 +10,39 @@ function SingleMovie( {movie, actor} ) {
         <h1>Single Movie</h1>
         <h2>{movie.title}</h2>
         <p>{movie.overview}</p>
-        <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />
+        <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} className="movie-image"/>
         <p>Runtime: {runtime} hours</p>
         <p>Release Date: {movie.release_date}</p>
         <p>Rating: {rating}%</p>
 
         {actor.cast === undefined ?
         <p>No Cast info Available</p>:
-        <section>
-          <h2>{actor.cast[0].name}</h2>
-          <img src={`https://image.tmdb.org/t/p/original/${actor.cast[0].profile_path}`} alt={actor.cast[0].name} />
+        <section className="cast-articles">
+          <article>
+            <h2>{actor.cast[0].name}</h2>
+            <img src={`https://image.tmdb.org/t/p/original/${actor.cast[0].profile_path}`} alt={actor.cast[0].name} className="movie-image"/>
+          </article>
+
+          <article>
+            <h2>{actor.cast[1].name}</h2>
+            <img src={`https://image.tmdb.org/t/p/original/${actor.cast[1].profile_path}`} alt={actor.cast[1].name} className="movie-image"/>
+          </article>
+
+          <article>
+            <h2>{actor.cast[2].name}</h2>
+            <img src={`https://image.tmdb.org/t/p/original/${actor.cast[2].profile_path}`} alt={actor.cast[2].name} className="movie-image"/>
+          </article>
+
+          <article>
+            <h2>{actor.cast[3].name}</h2>
+            <img src={`https://image.tmdb.org/t/p/original/${actor.cast[3].profile_path}`} alt={actor.cast[3].name} className="movie-image"/>
+          </article>
+
+          <article>
+            <h2>{actor.cast[4].name}</h2>
+            <img src={`https://image.tmdb.org/t/p/original/${actor.cast[4].profile_path}`} alt={actor.cast[4].name} className="movie-image"/>
+          </article>
+
         </section>
 }
 
